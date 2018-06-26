0b80286824dcb8ac54b2091783a278809b30c8d0
hodanroble <hodanroble@rochesterstemacademy.org>
2018-06-25 21:10:34 -0500

    routes added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require('path');
 
 // Sets up the Express App
 // =============================================================
@@ -25,12 +26,91 @@ app.use(bodyParser.json());
 // Static directory
 app.use(express.static("public"));
 
+//stripe configure
+var keyPublishable = process.env.PUBLISHABLE_KEY;
+var keySecret = process.env.SECRET_KEY;
+var stripe = require("stripe")("sk_test_dOFWIEyJc2fkOc8XzHtKTadE");
+
+//creating cookies and underline session
+var session = require('express-session');
+var passport = require('passport');
+
 // Routes
 // =============================================================
 // require("./routes/html-routes.js")(app);
 require("./routes/product-api-route.js")(app);
 require("./routes/review-api-route.js")(app);
 require("./routes/size-api-route.js")(app);
+var connection = require('./db/connection');
+var login = require('./routes/login');
+var register = require('./routes/register');
+var profile = require('./routes/profile');
+var scarves = require('./routes/scarves');
+var cart = require('./routes/cart');
+var charge = require('./routes/charge');
+
+//where passport auth will be set up
+require('./auth/setup');
+
+
+
+connection.connect();
+
+
+//sessionConfig that happens before passport will be. specifying cookie info
+
+var sessionConfig = {
+  //not shared with people. searches for it in the environment
+  secret: process.env.SECRET || 'super secrect key goes here',
+  key: 'user',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 30 * 50 * 1000, //30 mins. Cookie should only last 30min
+    secure: false
+  }
+}
+
+app.use(session(sessionConfig));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//no auth needed where route folders will go
+app.use('/login', login);
+app.use('/register', register);
+app.use('/person', profile);
+app.use('/scarf', scarves);
+app.use('/carts', cart);
+app.use('/charge', charge);
+app.get('/loginStatus', function (req,res){
+  res.send(req.isAuthenticated());
+})
+
+//following routes require auth
+app.use('/private', ensureAuthenticated);
+
+app.get('/private/secretInfo', function(req, res){
+  console.log('Sending secret info');
+  res.send('This is very secret');
+});
+function ensureAuthenticated(req, res, next){
+  console.log('Ensuring the user is authenticated');
+  if(req.isAuthenticated()){
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port', server.address().port);
+});
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
