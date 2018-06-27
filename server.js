1bf257d1381dd6e72040f40792a8dcf55a923493
ileysosman <ileys.osmann@gmail.com>
2018-06-26 19:16:32 -0500

    made first purchas on stripe we're rich

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 require("dotenv").config()
 var PUB_KEY = process.env.PUB_KEY
 var SECRET_KEY = process.env.SECRET_KEY
-console.log(process.env);
+console.log (SECRET_KEY)
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = require("stripe")(SECRET_KEY);
+
+const charge = stripe.charges.create(
+    {
+        amount: 999,
+        currency: 'usd',
+        source: 'tok_visa',
+        receipt_email: 'jenny.rosen@example.com',
+    }
+).then(res => {
+    console.log (res)
+})
