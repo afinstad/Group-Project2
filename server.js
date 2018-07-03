a1cd3b9e327b86858388216804810939a9e8b53d
hodanroble <hodanroble@rochesterstemacademy.org>
2018-07-02 21:05:52 -0500

    working stripe code

--- server.js
+++ server.js
@@ -2,16 +2,49 @@
 // *** Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require('path');
-var session = require('express-session');
-var passport = require('passport');
+var path = require("path");
+var session = require("express-session");
+var passport = require("passport");
+var stripe = require ("stripe")("rk_test_coIEq8ziNXdJcEMOmbs11M2T")
 
 // Requiring our models for syncing
 var db = require("./models");
 
 // Sets up the Express App
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
+// res.sendFile('index.html', {root : __dirname + '/public/views/index.html'});
+app.use(express.static(__dirname + '/public'));
+app.use (bodyParser.json()); // Using body parser
+app.use (bodyParser.urlencoded({extended: false}));
+app.get('/', function (req, res) {
+  res.sendfile('./public/views/index.html');
+});
+app.get('/', function (req, res) {
+  res.sendfile('payment.html', {
+
+  });
+}); // page not made yet that shows payment was made succesfully
+app.post ('/charge', function (req, res){
+  var token = req.body.stripeToken;
+  var chargeAmount = req.body.chargeAmount;
+  var charge = stripe.charges.create({
+    amount: chargeAmount,
+    currency:"usd",
+    source: token
+  }, function (err, charge){
+    if(err & err.type === "StripeCardError") {
+      console.log ("Your card was declined");
+    }
+  });
+  });
+  console.log ("your payment was succesful");
+  // res.redirect('/payment.html');
+  
+
+app.listen (3000, function (){
+  console.log ("Stripe is running");
+});
 
 // // Ileys
 // require("dotenv").config()
